7d6fde7daa99e3a0165c530efac6bc46b6165919
Kevin Krahn <koderkk@gmail.com>
2024-01-31 17:51:41 -0500

    Add support for more base model architectures

--- sentence_transformers/models/Transformer.py
+++ sentence_transformers/models/Transformer.py
@@ -91,14 +91,13 @@ class Transformer(nn.Module):
 
     def forward(self, features):
         """Returns token_embeddings, cls_token"""
-        trans_features = {"input_ids": features["input_ids"], "attention_mask": features["attention_mask"]}
-        if "token_type_ids" in features:
-            trans_features["token_type_ids"] = features["token_type_ids"]
+        feature_names = ["input_ids", "attention_mask", "token_type_ids", "char_input_mask", "word_input_mask"]
+        trans_features = { k: features[k] for k in feature_names if k in features }
 
         output_states = self.auto_model(**trans_features, return_dict=False)
         output_tokens = output_states[0]
 
-        features.update({"token_embeddings": output_tokens, "attention_mask": features["attention_mask"]})
+        features.update({"token_embeddings": output_tokens})
 
         if self.auto_model.config.output_hidden_states:
             all_layer_idx = 2
